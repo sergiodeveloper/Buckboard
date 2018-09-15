1a12895ae392a8a047eb23dfc52e5553862170e0
Mateus Tanaka <mateusht@ciandt.com>
2018-09-15 12:53:23 -0300

    Criando tela de bem-vindo e login

---
+++ app/src/main/java/br/unicamp/ft/s187064_m184082/buckboard/HomeActivity.java
@@ -0,0 +1,17 @@
+package br.unicamp.ft.s187064_m184082.buckboard;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+
+    public void openCreateNewUserView(View view) {
+    }
+}
--- app/src/main/java/br/unicamp/ft/s187064_m184082/buckboard/MainActivity.java
+++ app/src/main/java/br/unicamp/ft/s187064_m184082/buckboard/MainActivity.java
@@ -1,31 +1,29 @@
 package br.unicamp.ft.s187064_m184082.buckboard;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static int SPLASH_TIME_OUT = 2000;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        new Handler().postDelayed(new Runnable() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void run() {
+                Intent homeIntent = new Intent(MainActivity.this, HomeActivity.class);
+                startActivity(homeIntent);
+                finish();
             }
-        });
+        }, SPLASH_TIME_OUT);
     }
 
     @Override
