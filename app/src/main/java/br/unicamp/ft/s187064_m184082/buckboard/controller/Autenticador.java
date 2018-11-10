24de5da83330706952dd646c086c6df1eb327e38
sergiodeveloper <sergio-fh@hotmail.com>
2018-11-10 17:21:47 -0200

    Adicionando login com email

--- app/src/main/java/br/unicamp/ft/s187064_m184082/buckboard/controller/Autenticador.java
+++ app/src/main/java/br/unicamp/ft/s187064_m184082/buckboard/controller/Autenticador.java
@@ -1,4 +1,76 @@
 package br.unicamp.ft.s187064_m184082.buckboard.controller;
 
+import android.content.Context;
+import android.net.Uri;
+import android.support.annotation.NonNull;
+import android.telecom.Call;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import static android.content.ContentValues.TAG;
+
 public class Autenticador {
+
+    public static interface CallbackLogin {
+        void sucesso(String nome, String email, Uri foto);
+
+        void erro();
+    }
+
+    public static void entrarEmailSenha(CallbackLogin callbackLogin, String email, String senha) {
+        FirebaseAuth mAuth = FirebaseAuth.getInstance();
+
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if (currentUser != null) {
+            callbackLogin.sucesso(currentUser.getDisplayName(), currentUser.getEmail(), currentUser .getPhotoUrl());
+            return;
+        }
+
+        mAuth.signInWithEmailAndPassword(email, senha).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    FirebaseUser user = mAuth.getCurrentUser();
+
+                    callbackLogin.sucesso(user.getDisplayName(), user.getEmail(), user.getPhotoUrl());
+                } else {
+                    callbackLogin.erro();
+                }
+            }
+        });
+    }
+
+    public static void logout() {
+        FirebaseAuth mAuth = FirebaseAuth.getInstance();
+
+        mAuth.signOut();
+    }
+
+    public static void cadastrarUsuarioEmailSenha(CallbackLogin callbackLogin, String nome, String sobrenome,
+                                                  String sexo, String estadoCivil, String email, String senha) {
+        FirebaseAuth mAuth = FirebaseAuth.getInstance();
+
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if (currentUser == null) return;
+
+        mAuth.createUserWithEmailAndPassword(email, senha).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    FirebaseUser user = mAuth.getCurrentUser();
+
+                    callbackLogin.sucesso(user.getDisplayName(), user.getEmail(), user.getPhotoUrl());
+                } else {
+                    callbackLogin.erro();
+                }
+            }
+        });
+    }
 }
+
--- app/src/main/java/br/unicamp/ft/s187064_m184082/buckboard/view/activity/ActivityEntrar.java
+++ app/src/main/java/br/unicamp/ft/s187064_m184082/buckboard/view/activity/ActivityEntrar.java
@@ -1,12 +1,16 @@
 package br.unicamp.ft.s187064_m184082.buckboard.view.activity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import br.unicamp.ft.s187064_m184082.buckboard.R;
+import br.unicamp.ft.s187064_m184082.buckboard.controller.Autenticador;
 
 public class ActivityEntrar extends AppCompatActivity {
 
@@ -28,5 +32,29 @@ public class ActivityEntrar extends AppCompatActivity {
         Intent homeIntent = new Intent(ActivityEntrar.this, CreateUserActivity.class);
         startActivity(homeIntent);
 //        finish();
+    }
+
+    public void entrar(View view) {
+        String email = ((EditText) findViewById(R.id.email)).getText().toString();
+        String senha = ((EditText) findViewById(R.id.senha)).getText().toString();
+
+        Autenticador.CallbackLogin callbackLogin = new Autenticador.CallbackLogin() {
+            @Override
+            public void sucesso(String nome, String email, Uri foto) {
+                // Abrir main activity
+                Intent intent = new Intent(ActivityEntrar.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+
+            @Override
+            public void erro() {
+                Toast.makeText(ActivityEntrar.this, "Falha ao fazer login", Toast.LENGTH_SHORT).show();
+            }
+        };
+
+        Autenticador.entrarEmailSenha(callbackLogin, email, senha);
+
+        Toast.makeText(this, "Por favor aguarde...", Toast.LENGTH_SHORT).show();
     }
 }
