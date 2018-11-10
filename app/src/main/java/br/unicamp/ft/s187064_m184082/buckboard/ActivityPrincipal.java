960afdb173a29b4c0a7bd16e559c9fcd01ad96f5
sergiodeveloper <sergio-fh@hotmail.com>
2018-11-10 16:22:59 -0200

    Adicionando SignInActivity do google para entrar com o firebase

--- app/src/main/java/br/unicamp/ft/s187064_m184082/buckboard/ActivityPrincipal.java
+++ app/src/main/java/br/unicamp/ft/s187064_m184082/buckboard/ActivityPrincipal.java
@@ -15,6 +15,9 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class ActivityPrincipal extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
