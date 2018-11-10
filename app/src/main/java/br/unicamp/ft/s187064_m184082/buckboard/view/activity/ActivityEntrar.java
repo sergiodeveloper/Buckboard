a145e8cd51c71e1ea1521e6639a917b735e6dab9
sergiodeveloper <sergio-fh@hotmail.com>
2018-11-10 18:20:55 -0200

    Comentando checkbox lembrar senha

--- app/src/main/java/br/unicamp/ft/s187064_m184082/buckboard/view/activity/ActivityEntrar.java
+++ app/src/main/java/br/unicamp/ft/s187064_m184082/buckboard/view/activity/ActivityEntrar.java
@@ -6,6 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Toast;
 
